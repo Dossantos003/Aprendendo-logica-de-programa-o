9dd652b8b4ec39f9907434c0dae4c691afbccd94
GABRIEL DOS SANTOS NASCIMENTO <gabriel09santos@gmail.com>
2025-11-24 21:22:05 -0300

    Add files via upload

---
+++ Aprendendo Logica de Programação/operadorE.js
@@ -0,0 +1,9 @@
+let ingresso = true
+let maiorDeidade = true
+
+if (ingresso && maiorDeidade) {
+    alert("Acesso liberado")
+}
+else {
+    alert("Acesso negado")
+}
---
+++ Aprendendo Logica de Programação/operadorOU.js
@@ -0,0 +1,8 @@
+let temIngresso = false
+let nomeNalista = false
+//true              true
+if(temIngresso || nomeNalista) {
+    alert("Entrada permitida!")
+}else{
+    alert('Entrada negada')
+}
